f269177cc8db07262b080fd184586fd5c5f1113e
Fábio Henriques <fabio.lacerda@outlook.com>
2018-03-18 00:12:35 -0300

    Integrates the Books API with Bookshelf components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 import { CssBaseline, AppBar, Toolbar, Typography } from 'material-ui';
 import Bookshelf from './components/Bookshelf'
+import { getAll } from './BooksAPI'
 
 class App extends Component {
+  state = {
+    currentlyReadingBooks: [],
+    wantToReadBooks: [],
+    readBooks: []
+  }
+
+  filterByShelf = (books, shelf) => {
+    return books.filter((book) => book.shelf === shelf)
+  }
+
+  componentDidMount () {
+    getAll()
+      .then((books) => {
+        this.setState({
+          currentlyReadingBooks: this.filterByShelf(books, "currentlyReading"),
+          wantToReadBooks: this.filterByShelf(books, "wantToRead"),
+          readBooks: this.filterByShelf(books, "read")
+        })
+      });
+  }
+
   render () {
+    const { currentlyReadingBooks, wantToReadBooks, readBooks } = this.state
+
     return (
       <div>
         <CssBaseline />
@@ -16,7 +40,9 @@ class App extends Component {
           </Toolbar>
         </AppBar>
         <div className="main-content">
-          <Bookshelf title="Currently Reading" />
+          <Bookshelf title="Currently Reading" books={currentlyReadingBooks} />
+          <Bookshelf title="Want to Read" books={wantToReadBooks} />
+          <Bookshelf title="Read" books={readBooks} />
         </div>
       </div>
     );
