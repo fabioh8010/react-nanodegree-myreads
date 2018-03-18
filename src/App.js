ac40426a9017da3441237598f30b71d5ac18521b
Fábio Henriques <fabio.lacerda@outlook.com>
2018-03-17 22:18:22 -0300

    Adds an app bar with title to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CssBaseline, AppBar, Toolbar, Typography } from 'material-ui';
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <h1>My Reads</h1>
+      <div>
+        <CssBaseline />
+        <AppBar position="fixed" color="primary">
+          <Toolbar>
+            <Typography className="app-bar" variant="title" color="inherit">
+              My Reads
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
